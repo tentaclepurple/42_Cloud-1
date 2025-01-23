e2e754ca1bbf48f8d125583cf25d4d0aa04e940c
tentaclepurple <wolfframio@gmail.com>
2025-01-24 00:55:21 +0100

    phpmyadmin ok, added redirections, enable ufw

--- inception/srcs/requirements/wordpress/tools/run.sh
+++ inception/srcs/requirements/wordpress/tools/run.sh
@@ -3,6 +3,7 @@
 sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php/7.3/fpm/pool.d/www.conf
 
 cat > /var/www/wordpress/wp-config.php << EOF
+
 <?php
 define( 'DB_NAME', '${MYSQL_DATABASE}' );
 define( 'DB_USER', '${MYSQL_USER}' );
@@ -10,7 +11,6 @@ define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );
 define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
-
 define( 'AUTH_KEY',         'put your unique phrase here' );
 define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
 define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
@@ -36,6 +36,6 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --path=$WP_PATH --allow-root
 wp core install --path=$WP_PATH --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --skip-email --allow-root
 wp theme install teluro --path=$WP_PATH --activate --allow-root
-wp user create leon leon@le.on --role=author --path=$WP_PATH --user_pass=leon --allow-root
+wp user create john john@con.h --role=author --path=$WP_PATH --user_pass=johnconh --allow-root
 
 /usr/sbin/php-fpm7.3 -F
